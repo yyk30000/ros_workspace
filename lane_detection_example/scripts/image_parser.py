96923fe1e18fc45f1c73f6f3ad3fad805462801c
yangwansuk <yyk30000@gmail.com>
2020-07-17 13:56:24 +0900

    오전반 작업분

--- lane_detection_example/scripts/image_parser.py
+++ lane_detection_example/scripts/image_parser.py
@@ -2,55 +2,72 @@
 
 import rospy
 import cv2
-import numpy as np 
+import numpy as np
 import os, rospkg
-from utills import warp_image
-
-
+import json
 
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridgeError
+from utills import BEVTransform
 
 
 class IMGParser:
     def __init__(self):
+        self.image_sub =rospy.Subscriber("/image_jpeg/compressed",CompressedImage,self.callback)
 
-        self.image_sub = rospy.Subscriber("/image_jpeg/compressed", CompressedImage, self.callback)
+        self.img_wlane=None
 
-    def callback(self, msg):
+
+    def callback(self,msg):
         try:
-            np_arr = np.fromstring(msg.data , np.uint8)
-            img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            np_arr = np.fromstring(msg.data,np.uint8)
+            img_bgr =cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
         except CvBridgeError as e:
             print(e)
-        self.img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
 
-        lower_wlane = np.array([20,0,240])
-        upper_wlane = np.array([40,15,255])
-        img_wlane=cv2.inRange(self.img_hsv, lower_wlane, upper_wlane)
-        img_wlane=cv2.cvtColor(img_wlane ,cv2.COLOR_GRAY2BGR)
-        img_concat = np.concatenate([img_bgr, self.img_hsv, img_wlane],axis=1)
+        img_hsv =cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV)
 
-        cv2.imshow("Image window",img_concat)
-        cv2.waitKey(1)
+        lower_wlane=np.array([0,0,220])
+        upper_wlane=np.array([40,15,255])
 
-    # def mouseRGB(self,event,x,y,flags,param):
-    #     if event == cv2.EVENT_LBUTTONDOWN:
-    #         colorsB= self.img_hsv[y,x,0]
-    #         colorsG= self.img_hsv[y,x,1]
-    #         colorsR= self.img_hsv[y,x,2]
-    #         colors = self.img_hsv[y,x]
-    #         print("Red: ", colorsR)
-    #         print("Green: ", colorsG)
-    #         print("Blue: ", colorsB)
-    #         print("BGR format: ", colors)
-    #         print("Coordinates of pixel: X:",x,"Y: ",y)
+        self.img_wlane=cv2.inRange(img_hsv,lower_wlane,upper_wlane)
 
-if __name__ == '__main__' :
+        
+
+if __name__ =='__main__' :
+    rp = rospkg.RosPack()
+
+    currentPath =rp.get_path("lane_detection_example")
+
+    with open(os.path.join(currentPath,'sensor/sensor_params.json'),'r') as fp:
+        sensor_params =json.load(fp)
+    
+    params_cam =sensor_params["params_cam"]
 
     rospy.init_node('image_parser',anonymous=True)
 
-    image_parser = IMGParser()
+    image_parser =IMGParser()
+    bev_op =BEVTransform(params_cam=params_cam)
 
-    rospy.spin()        
+    rate =rospy.Rate(30)
 
+    while not rospy.is_shutdown():
+        
+        if image_parser.edges is not None:
+
+            img_warp = bev_op.warp_bev_img(image_parser.edges)
+            lane_pts = bev_op.recon_lane_pts(image_parser.edges)
+            
+            x_pred, y_pred_l , y_pred_r = curve_learner.fit_curve(lane_pts)
+           
+            xyl, xyr = bev_op.project_lane2img(x_pred, y_pred_l, y_pred_r)
+            
+            img_warp1 = draw_lane_img(img_warp, xyl[: ,0].astype(np.int32),
+                                                xyl[: ,1].astype(np.int32),
+                                                xyr[: ,0].astype(np.int32),
+                                                xyr[: ,1].astype(np.int32))
+            cv2.imshow("Image window",img_warp1)
+            cv2.waitKey(1)
+
+            rate.sleep()
+        
--- lane_detection_example/scripts/utills.py
+++ lane_detection_example/scripts/utills.py
@@ -77,12 +77,12 @@ def project2img_mtx(params_cam):
 
     return R_f                
 
-class BENTransform:
+class BEVTransform:
     def __init__(self, params_cam, xb=1.0,zb=1.0 ):
 
         self.xb = xb
         self.zb = zb
-        self.theta = np.def2rad(params_cam["PITCH"])
+        self.theta = np.deg2rad(params_cam["PITCH"])
         self.width = params_cam["WIDTH"]
         self.height = params_cam["HEIGHT"]
 
@@ -91,32 +91,88 @@ class BENTransform:
             self.fc_y =params_cam["HEIGHT"]/(2*np.tan(np.deg2rad(params_cam["FOV"]/2)))
 
             self.alpha_c = np.arctan2(params_cam["WIDTH"]/2,self.fc_y)
-            self.fc._x = self.fc_y
+            self.fc_x = self.fc_y
 
         else:
             self.alpha_r = np.deg2rad(params_cam["FOV"]/2)
             self.fc_x =params_cam["HEIGHT"]/(2*np.tan(np.deg2rad(params_cam["FOV"]/2)))
 
-            self.alpha_c = np.arctan2(params_cam["WIDTH"]/2,self.fc_y)
-            self.fc._y = self.fc_x
+            self.alpha_c = np.arctan2(params_cam["WIDTH"]/2,self.fc_x)
+            self.fc_y = self.fc_x
 
         self.h = params_cam["Z"]
+        self.x = params_cam["X"]
 
         self.n =float(params_cam["WIDTH"])
         self.m =float(params_cam["HEIGHT"])
 
-        self.RT_b2g = np.matmul(np.matmul(translationMtx(xb,0,zb),rotationMtx(np.deg2rad(-90),0,0)),rotationMtx(0,0,np.deg2rad(180)))
+        self.RT_b2g = np.matmul(np.matmul(translationMtx(xb,0,zb),rotationMtx(np.deg2rad(-90),0,0)),
+                                rotationMtx(0,0,np.deg2rad(180)))
 
         self.build_tf(params_cam)
 
-def calc_Xy_Yu(self,U,V):
+    def calc_Xv_Yu(self,U,V):
+        Xv = self.h*(np.tan(self.theta)*(1-2*(V-1)/(self.m-1))*np.tan(self.alpha_r)-1)/\
+            (-np.tan(self.theta)+(1-2*(V-1)/(self.m-1))*np.tan(self.alpha_r))
+            
+        Yu=(1-2*(U-1)/(self.n-1))*Xv*np.tan(self.alpha_c)
+            
+        return Xv, Yu
 
-    Xv = self.h*(np.tan(self.theta)*(1-2*(V-1)/(self.m-1))*np.tan(self.alpha_r)-1)/\
-        (-np.tan(self.theta)+(1-2*(V-1)/(self.m-1))*np.tan(self.alpha_r))
+    def build_tf(self,params_cam):
 
-    Yu=(1-2*(U-1)/(self.n-1))*Xv*np.tan(self,alpha_c))
+        v=np.array([params_cam["HEIGHT"]*0.5, params_cam["HEIGHT"]]).astype(np.float32)
+        u=np.array([0,params_cam["WIDTH"]]).astype(np.float32)
 
-    return Xv, Yu
+        U,V =np.meshgrid(u,v)
+
+        Xv,Yu = self.calc_Xv_Yu(U,V)
+
+        xyz_g = np.concatenate([Xv.reshape([1,-1]) + params_cam["X"],
+                                Yu.reshape([1,-1]),
+                                np.zeros_like(Yu.reshape([1,-1])),
+                                np.ones_like(Yu.reshape([1,-1]))], axis=0)
+
+        xyz_bird = np.matmul(np.linalg.inv(self.RT_b2g),xyz_g) 
+
+        xc,yc,zc = xyz_bird[0,:].reshape([1,-1]),xyz_bird[1,:].reshape([1,-1]), xyz_bird[2,:].reshape([1,-1])
+
+        proj_mtx= project2img_mtx(params_cam)
+
+        xn , yn = xc/zc, yc/zc
+        xy1 = np.matmul(proj_mtx, np.concatenate([xn,yn,np.ones_like(xn)], axis=0))
+
+        xy1= xy1[0:2,:].tan
+        src_pts = np.concatenate([U.reshape([-1,1]),V.reshape([-1,1])],axis=1).astype(np.float32)
+        dst_pts = xy1.astype(np.float32)
+
+        self.perspective_tf = cv2.getPerspectiveTransform(src_pts,dst_pts)          
+
+    def warp_bev_img(self,img):
+        img_warp = cv2.warpPerspective(img,self.perspective_tf,(self.width, self.height), flags=cv2.INTER_LINEAR)
+        return img_warp
+
+    def recon_lane_pts(self,img):
+        img[:int(0.5*self.height), :]=0
+        if cv2.countNonZero(img) != 0 :
+
+            UV_mark = cv2.findNonZero(img).reshape([-1,2])
+
+            U, V = UV_mark[:, 0].reshape([-1,1]), UV_mark[:,1].reshape([-1,1])
+
+            Xv,Yu = self.calc_Xv_Yu(U, V )
+
+            xyz_g = np.concatenate([Xv.reshape([1,-1])+ self.x,
+                                  Yu.reshape([1,-1]),
+                                np.zeros_like(Yu.reshape([1,-1])),
+                                np.ones_like(Yu.reshape([1,-1]))],axis=0)  
+
+
+            xyz_g = xyz_g[:,xyz_g[0,:]>=0]
+        else:
+            xyz_g = np.zeros((4,10))
+
+        return xyz_g                                         
 
         
 
