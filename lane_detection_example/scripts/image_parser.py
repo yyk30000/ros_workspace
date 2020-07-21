418591a066ea76e567c466a4e9b0bee797278979
yangwansuk <yyk30000@gmail.com>
2020-07-21 15:25:16 +0900

    신호등 인지

--- lane_detection_example/scripts/image_parser.py
+++ lane_detection_example/scripts/image_parser.py
@@ -13,7 +13,7 @@ from utils import BEVTransform,CURVEFIt,draw_lane_img ,purePursuit,STOPLineEstim
 
 class IMGParser:
     def __init__(self):
-        self.image_sub =rospy.Subscriber("/imag_jpeg/compressed",CompressedImage,self.callback)
+        self.image_sub =rospy.Subscriber("/image_jpeg/compressed",CompressedImage,self.callback)
 
         self.img_wlane=None
 
@@ -25,13 +25,52 @@ class IMGParser:
         except CvBridgeError as e:
             print(e)
 
-        img_hsv =cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV)
+        self.img_hsv =cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV)
 
         lower_wlane=np.array([0,0,220])
         upper_wlane=np.array([40,15,255])
-
         
-        self.img_wlane=cv2.inRange(img_hsv,lower_wlane,upper_wlane)
+        self.img_wlane=cv2.inRange(self.img_hsv,lower_wlane,upper_wlane)
+
+        h= img_bgr.shape[0]
+        w= img_bgr.shape[1]
+
+        lower_sig_y =np.array([20,250,250])
+        upper_sig_y =np.array([25,255,255])
+        lower_sig_r =np.array([0,250,250])
+        upper_sig_r =np.array([5,255,255])
+        lower_sig_g =np.array([50,250,250])
+        upper_sig_g =np.array([65,255,255])
+
+        img_r =cv2.resize(cv2.inRange(self.img_hsv,lower_sig_r,upper_sig_r),(w/2,h/2))
+        img_y =cv2.resize(cv2.inRange(self.img_hsv,lower_sig_y,upper_sig_y),(w/2,h/2))
+        img_g =cv2.resize(cv2.inRange(self.img_hsv,lower_sig_g,upper_sig_g),(w/2,h/2))
+
+        img_r[int(h/3/2):,:]=0
+        img_y[int(h/3/2):,:]=0
+        img_g[int(h/3/2):,:]=0
+
+        img_concat=np.concatenate([img_r,img_y,img_g],axis=1)
+        cv2.imshow("Image window",img_concat)
+        cv2.waitKey(1)
+        # cv2.namedWindow('mouseRGB')
+        # cv2.imshow('mouseRGB',self.img_hsv)
+        # cv2.setMouseCallback('mouseRGB',self.mouseRGB)
+        # cv2.waitKey(1)
+    
+
+    def mouseRGB(self,event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            colorsB =self.img_hsv[y,x,0]
+            colorsG =self.img_hsv[y,x,1]
+            colorsR =self.img_hsv[y,x,2]
+            colors =self.img_hsv[y,x]
+            print("Red: ",colorsR)
+            print("Green: ",colorsG)
+            print("Blue: ",colorsB)
+            print("BRG Format: ",colors)
+            print("Coordinates of pixel. x: ",x,"Y: ",y)
+
         
 
         
@@ -60,7 +99,7 @@ if __name__ =='__main__' :
         
         
         if image_parser.img_wlane is not None:
-
+            
             img_warp =bev_op.warp_bev_img(image_parser.img_wlane)
             lane_pts =bev_op.recon_lane_pts(image_parser.img_wlane)
 
@@ -72,8 +111,8 @@ if __name__ =='__main__' :
            
             x_pred,y_pred_l,y_pred_r =curve_learner.fit_curve(lane_pts)
 
-            ctrler.steering_angle(x_pred,y_pred_l,y_pred_r)
-            ctrler.pub_cmd()
+            # ctrler.steering_angle(x_pred,y_pred_l,y_pred_r)
+            # ctrler.pub_cmd()
             
 
             xyl,xyr = bev_op.project_lane2img(x_pred,y_pred_l,y_pred_r)
@@ -85,10 +124,11 @@ if __name__ =='__main__' :
             
            
             img_lane = cv2.cvtColor(image_parser.img_wlane, cv2.COLOR_GRAY2BGR)
-            img_concat = np.concatenate([img_lane, img_warp], axis=1)
-           
-            cv2.imshow("Image window ",img_warp1)
-            cv2.waitKey(1)
+            # img_concat = np.concatenate([img_lane, img_warp], axis=1)
+
+            
+            # cv2.imshow("Image window ",img_warp1)
+            # cv2.waitKey(1)
 
             rate.sleep()
     
