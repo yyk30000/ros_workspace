9d06e435b9d22638f2955ccf7b48238fa2e98820
yangwansuk <yyk30000@gmail.com>
2020-08-10 14:28:33 +0900

    ttt

--- control_planner/scripts/path_pub.py
+++ control_planner/scripts/path_pub.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-
+import numpy as np
 import rospy
 import rospkg
 from sensor_msgs.msg import LaserScan,PointCloud,Imu
@@ -13,6 +13,41 @@ from nav_msgs.msg import Odometry,Path
 
 import tf
 from tf.transformations import euler_from_quaternion,quaternion_from_euler
+class velocityPlanning:
+    def __init__(self,car_max_speed,road_friction):
+        self.car_max_speed=car_max_speed
+        self.road_friction=road_friction
+
+    def curveBasedVelocity(self,global_path,point_num):
+        out_vel_plan=[]
+        for i in range(0,point_num):
+            out_vel_plan.append(self.car_max_speed)
+        for i in range(point_num,len(global_path.poses)-point_num):
+            x_list=[]
+            y_list=[]
+            for box in range(-point_num,point_num):
+                x=global_path.poses[i+box].pose.position.x
+                y=global_path.poses[i+box].pose.position.y
+                x_list.append([-2*x, 2*y,1]) 
+                y_list.append(-(x*x)-(y*y))
+
+            x_matrix=np.array(x_list)      
+            y_matrix=np.array(y_list)  
+            x_trans=x_matrix.T
+
+            a_matrix=np.linalg.inv(x_trans.dot(x_matrix)).dot(x_trans).dot(y.matrix)
+            a=a_matrix[0]
+            b=a_matrix[1]
+            c=a_matrix[2]
+            r=sqrt(a*a+b*b-c)
+            v_max=sqrt(r*9.8*self.road_friction)*3.6
+            if v_max>self.car_max_speed :
+                v_max=self.car_max_speed
+            out_vel_plan.append(v_max)
+
+        for i in range(len(global_path.poses)-point_num,len(global_path.poses)):
+            out_vel_plan.append(self.car_max_speed)
+        return out_vel_plan        
 
 class path_pub:
 
